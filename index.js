6a93a223444707d9406726a4857d4498acb0f86f
ryunosukesatou <96858418+ryunosukesatou@users.noreply.github.com>
2022-05-02 03:48:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ $(function() {
 	$('.slider').slick({
 		fade:true,//切り替えをフェードで行う。初期値はfalse。
 		autoplay: true,//自動的に動き出すか。初期値はfalse。
-		autoplaySpeed: 5000,//次のスライドに切り替わる待ち時間
+		autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
 		speed:1000,//スライドの動きのスピード。初期値は300。
 		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
 		slidesToShow: 1,//スライドを画面に3枚見せる
